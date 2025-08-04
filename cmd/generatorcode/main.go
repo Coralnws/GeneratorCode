821fec2bb3586eafb02b0f2d1c14fec9783429ae
Coral <101916048+Coralnws@users.noreply.github.com>
2025-08-04 21:54:07 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/generatorcode/main.go
@@ -0,0 +1,23 @@
+// cmd/generatorcode/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"generatorcode/internal/generatorcode"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := generatorcode.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
